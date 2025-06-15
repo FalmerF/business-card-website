7f1f3d1c8debf7d3699e01766ebfaca2d5206174
Falmer <foulorown@gmail.com>
2025-06-15 18:56:28 +0300

    Initial

---
+++ src/main/java/ru/ilug/business_card_website/BusinessCardWebsiteApplication.java
@@ -0,0 +1,13 @@
+package ru.ilug.business_card_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessCardWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessCardWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/ilug/business_card_website/BusinessCardWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ilug.business_card_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessCardWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
