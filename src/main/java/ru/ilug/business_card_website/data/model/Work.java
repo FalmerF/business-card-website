c92cec50285f909b8e6967ffc506e62ad0622d2b
Falmer <foulorown@gmail.com>
2025-06-18 15:51:10 +0300

    Add work stack

--- src/main/java/ru/ilug/business_card_website/data/model/Work.java
+++ src/main/java/ru/ilug/business_card_website/data/model/Work.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -15,5 +17,6 @@ public class Work {
     private String position;
     private String period;
     private String tasks;
+    private List<String> stack;
 
 }
